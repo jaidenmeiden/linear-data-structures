cbb01a903cfe9576035c27860a0d6abd9492eb6d
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-11-24 16:51:16 +0100

    feat: Double practice

---
+++ Nodes/doublePractice.py
@@ -0,0 +1,17 @@
+from customTwoWayNode import CustomTwoWayNode
+
+# Create a doubly linked list with one node
+head = CustomTwoWayNode(1)
+tail = head
+
+# Add four node to the end of the linked list
+for data in range(2, 6):
+    tail.next = CustomTwoWayNode(data, tail)
+    tail = tail.next
+
+# Print the contents of the linked list in reverse order
+probe = tail
+
+while probe is not None:
+    print(probe.data)
+    probe = probe.previous
