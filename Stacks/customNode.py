fcccd3ffe1be7f8bae7a8cfab2676dd3ac72ced2
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-11-26 21:18:00 +0100

    feat: Add stack class

---
+++ Stacks/customNode.py
@@ -0,0 +1,11 @@
+"""
+Nodes y singly linked list
+
+The node methods should be incorporated into the Node class.
+"""
+
+
+class CustomNode(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
